6f8153711521a96b670d31a989b9348a1e7a9f2e
Rylan Bauermeister <apple@MacBook-Pro-6.local>
2019-05-22 17:37:24 -0700

    Done.

---
+++ backend_project/app/controllers/application_controller.rb
@@ -0,0 +1,41 @@
+class ApplicationController < ActionController::API
+  before_action :authorized
+
+
+  def encode_token(payload)
+    JWT.encode(payload, ENV['JWT_DECODER'])
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+
+    if auth_header
+      token = auth_header.split(" ")[1]
+
+      begin
+        JWT.decode(token, ENV['JWT_DECODER'], true, algorithm: 'HS256')[0]
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0][user_id]
+      @user = User.findBy(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+end
